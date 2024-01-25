f7770f1bf52dd2e5dda0de935c0db4aa7209ec74
BigPo101 <123412376+BigPo101@users.noreply.github.com>
2024-01-25 12:59:08 -0500

    Update products.js

--- products.js
+++ products.js
@@ -8,10 +8,10 @@ const products = [
     },
     {
         "id": 2,
-        "name":" LD02 LOUNGE CHAIR",
-        "price": 250,
-        "image": "images/2.png",
-        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+        "name":"Skittles",
+        "price": 2,
+        "image": "images/skittles.png",
+        "description": "Choose from Regular, Sour, and Wild Berry Skittles"
     },
     {
         "id": 3,
